d074366705ac0401d45c2f6c6a40231a05ff3140
Morgan-Sell <morganpsell@gmail.com>
2023-12-09 11:56:07 -0500

    implement chat history storage in documents helper

--- backend/core.py
+++ backend/core.py
@@ -1,10 +1,10 @@
 import os
-from typing import Any
+from typing import Any, List, Tuple
 from dotenv import load_dotenv
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
 from langchain.vectorstores import Pinecone
 import pinecone
 
@@ -17,20 +17,19 @@ pinecone.init(
 )
 
 
-def run_llm(query: str) -> Any:
+def run_llm(query: str, chat_history: List[Tuple[str, Any]] = []) -> Any:
     embeddings = OpenAIEmbeddings()
     docsearch = Pinecone.from_existing_index(
         index_name=os.environ["INDEX_NAME"],
         embedding=embeddings,
     )
     chat = ChatOpenAI(verbose=True, temperature=0)
-    qa = RetrievalQA.from_chain_type(
-        llm=chat,
-        chain_type="stuff",
-        retriever=docsearch.as_retriever(),
-        return_source_documents=True,
+
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
     )
-    return qa({"query": query})
+
+    return qa({"question": query, "chat_history": chat_history})
 
 
 if __name__ == "__main__":
